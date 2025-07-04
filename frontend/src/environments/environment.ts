5da190444af5d96a2a8302067ba4ed463683c047
Ahmed Shaarawy <ahmedshaarawy891@gmail.com>
2025-07-04 12:48:29 +0300

    Refactor SCSS styles by removing paragraph margin from article details and adding margin to empty paragraphs in base styles. Update environment configuration to include a commented production base URL.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:1337'
+  baseUrl: 'http://localhost:1337',
+  // baseUrl: 'https://worthy-love-9e46cd36dc.strapiapp.com',
 };
